9d673d2ed597961b6dfab4ad8046be4b207d599e
Christopher Little <hephistocles@gmail.com>
2014-08-28 15:55:41 +0100

    set up a bit of structure

---
+++ routes.js
@@ -0,0 +1,14 @@
+/*jshint unused:true, bitwise:true, eqeqeq:true, undef:true, latedef:true, eqnull:true */
+/* global __dirname, module */
+
+var app = module.parent.exports.app;
+var express = module.parent.exports.express;
+
+// serve an html page on the root url
+// we have registered a default views directory (/views) and renderer (ejs) so only "chat.html" is necessary
+app.get('/', function(req, res) {
+	res.render("home.jade");
+});
+
+// serve static files in the 'assets' folder directly from the root
+app.use('/', express.static(__dirname + '/assets'));
