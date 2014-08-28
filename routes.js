adf27c9f8c3e6331fe6129f243896f51e2925fba
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2014-08-28 20:30:37 +0100

    custom css, arrow image, updated routes.js, templates

--- routes.js
+++ routes.js
@@ -7,7 +7,23 @@ var express = module.parent.exports.express;
 // serve an html page on the root url
 // we have registered a default views directory (/views) and renderer (ejs) so only "chat.html" is necessary
 app.get('/', function(req, res) {
-	res.render("home.jade");
+	res.render("index.jade");
+});
+
+app.get('/paper/:id/discussion', function(req, res) {
+	res.render("paper-discussion.jade");
+});
+
+app.get('/paper/:id', function(req, res) {
+	res.render("paper.jade");
+});
+
+app.get('/paper', function(req, res) {
+	res.render("paper.jade");
+});
+
+app.get('/user', function(req, res) {
+	res.render("user.jade", user);
 });
 
 // serve static files in the 'assets' folder directly from the root
