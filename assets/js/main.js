befef3658b1f815f46a1717f771eb8f682ddf95a
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2014-08-29 10:05:01 +0100

    discuss

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    function show(button) {
+        button.text("Show Less");
+
+        var boxToAnimate = button.next();
+
+        boxToAnimate.html(boxToAnimate.data("full-html"));
+        var curHeight = boxToAnimate.height(),
+            autoHeight = boxToAnimate.css('height', 'auto').height();
+        boxToAnimate.height(curHeight).animate({
+            height: autoHeight
+        }, 'fast', function() {
+            boxToAnimate.height("auto");
+        });
+        button.next().animate("height", "show");
+        button.off("click").click(function() {
+            hide(button);
+        });
+    }
+
+    function hide(button) {
+        button.text("Show More");
+        var boxToAnimate = button.next();
+        boxToAnimate.data("full-html", boxToAnimate.html());
+        boxToAnimate.animate({
+            height: "20px"
+        }, 'fast', function() {
+            boxToAnimate.text(boxToAnimate.text().substr(0,50) + "...");
+
+        });
+        button.off("click").click(function() {
+            show(button);
+        });
+    }
+    $(".showhide-btn").each(function(i, btn) {
+        hide($(btn));
+    });
+});
